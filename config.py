2a84ca1eda4a92b592fb6201ec1e09ae1f4524c8
Devpareek8 <165389457+Devpareek8@users.noreply.github.com>
2025-04-01 11:36:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ from os import environ
 # For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
 class Config:
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    API_ID = environ.get("API_ID", "24942826")
+    API_HASH = environ.get("API_HASH", "e3e2f3b65ef58634139ccd27d6b7d8cb")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7765423827:AAFzlF282Af9ZIch8yRjtzLpX4fzZOQD6no") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Dec:Dec@cluster0.b6vd9iu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Dec")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6914788581').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002382972200'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "MoviesEmpire_Backup") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
     
